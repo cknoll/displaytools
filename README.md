6de5963485ef657df424e89b20698456fd9cae8f
Carsten Knoll <CarstenKnoll@gmx.de>
2015-12-01 21:34:55 +0100

    add example notebook and implement printing of left hand side

--- README.md
+++ README.md
@@ -7,13 +7,19 @@ It enables the user to trigger the display the content of a newly assigned varia
 
 ---
 
-**Installation:**
+## Installation:
 
-The file displaytools.py must be placed somwhere in $PYTHONPATH
+The file displaytools.py must be placed somewhere in $PYTHONPATH or simply
+in the same directory as the notebook where it shall be included.
 
 ---
 
-**Usage:**
+## Example:
+See this [IPython notebook](http://nbviewer.ipython.org/github/cknoll/displaytools/blob/master/example1.ipynb)
+
+---
+
+## Usage:
 
 Load this extension with `%load_ext displaytools` or `%reload_ext displaytools`
 The latter is usefull for debugging.
@@ -26,15 +32,21 @@ Due to the special comment `##` the extension inserts the line `display(my_rando
 before it is passed to the interpreter, i.e. before its execution.
 
 That way, additional output is generated, which makes the notebook is more comprehensible
-(beacause the reader knows the content of `my_random_variable`). It saves the typing effort and the code
+(because the reader knows the content of `my_random_variable`). It saves the typing effort and the code
 duplication of manually adding `display(my_random_variable)`.
 
 Of course there can be several such in vocations within one cell.
 
+If there is no assignment taking place, `##` nevertheless causes the display of the respective result.
+
+
+The special comment `##:` additionally triggers the printing of the left hand side of the assignment.
+
 ---
 
-**Security Advice**
+## Security Advice
 
 Because, the extension manipulates the source code before its execution it might cause unwanted and strange behavior.
+Thus, this program is distributed in the hope that it will be useful, *but without any warrenty*.
 
----
+
--- displaytools.py
+++ displaytools.py
@@ -26,7 +26,7 @@ the content of `my_random_variable`. It saves the typing effort and the code
 duplication of manually adding `display(my_random_variable)`.
 """
 
-# todo use sp.Eq(sp.Symbol('Z1'), theta, evaluate=False) to get better formatting
+# todo maybe use sp.Eq(sp.Symbol('Z1'), theta, evaluate=False) to get better formatting
 
 
 import new
@@ -34,11 +34,12 @@ import new
 from IPython.display import display
 
 special_comment = '##'
+special_comment_lhs = '##:'
 
 def insert_disp_lines(raw_cell):
     lines = raw_cell.split('\n')
     N = len(lines)
-
+    
     # iterate from behind -> insert does not change the lower indices
     for i in xrange(N-1, -1, -1):
         line = lines[i]
@@ -58,15 +59,65 @@ def insert_disp_lines(raw_cell):
             idx = line.index('##')
             lines[i] = 'display(%s); print("___")' % line[:idx]
             
+        elif line.endswith(special_comment_lhs) and line.count(' = ') == 1 and not line[0] in [' ', '#']:
+            idx = line.index(' = ')
+            var_str = line[:idx].strip()
+
+            new_line = 'custom_display("%s", %s); print("---")' % (var_str, var_str)
+            lines.insert(i+1, new_line)
+            
+            
 
     new_raw_cell = "\n".join(lines)
 
     return new_raw_cell
 
+def custom_display(lhs, rhs):
+    """
+    lhs: left hand side
+    rhs: right hand side
+    
+    This function serves to inject the string for the left hand
+    """
+    
+    # This code is mainly copied from IPython/display.py
+    # (IPython version 2.3.0)
+    kwargs = {}
+    raw = kwargs.get('raw', False)
+    include = kwargs.get('include')
+    exclude = kwargs.get('exclude')
+    metadata = kwargs.get('metadata')
+
+    from IPython.core.interactiveshell import InteractiveShell
+    from IPython.core.displaypub import publish_display_data
+
+    format = InteractiveShell.instance().display_formatter.format
+    format_dict, md_dict = format(rhs, include=include, exclude=exclude)
+    
+    # exampl format_dict (for a sympy expression):
+    # {u'image/png': '\x89PNG\r\n\x1a\n\x00 ...\x00\x00IEND\xaeB`\x82',
+    #  u'text/latex': '$$- 2 \\pi \\sin{\\left (2 \\pi t \\right )}$$',
+    # u'text/plain': u'-2\u22c5\u03c0\u22c5sin(2\u22c5\u03c0\u22c5t)'}
+    
+    # it is up to IPython which item value is finally used
+    
+    # now merge the lhs into the dict:
+    
+    if not isinstance(lhs, basestring):
+        raise TypeError('unexpexted Type for lhs object: %s' %type(lhs))
+    
+    new_format_dict = {}
+    for key, value in format_dict.items():
+        if 'text/' in key:
+            new_value = lhs+' := '+value
+            new_format_dict[key] = new_value
+        else:
+            # this happens e.g. for mime-type (i.e. key) 'image/png'
+            new_format_dict = format_dict
+    publish_display_data('display', new_format_dict, md_dict)
+
 
 def load_ipython_extension(ip):
-    #print "hello world"
-    #print dir(ip)
 
     def new_run_cell(self, raw_cell, *args, **kwargs):
 
@@ -87,4 +138,5 @@ def load_ipython_extension(ip):
         
     ip.run_cell = new.instancemethod(new_run_cell, ip)
     ip.user_ns['display'] = display
+    ip.user_ns['custom_display'] = custom_display
 
